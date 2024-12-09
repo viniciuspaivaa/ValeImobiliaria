a2ebf718e17cd66058c85dfe26e4a17c47180e5f
Vinicius <vinicius.paiva2@outlook.com>
2024-12-09 09:09:57 -0300

    Estilizando box flutuante index

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,5 +2,5 @@ const searchRoomsInput = document.getElementById('search-rooms-input');
 const searchRoomsBox = document.getElementById('search-rooms-box');
 
 searchRoomsInput.addEventListener('click', () => {
-    searchRoomsBox.style.display = searchRoomsBox.style.display == 'none' ? searchRoomsBox.style.display = 'block' : searchRoomsBox.style.display = 'none';
+    searchRoomsBox.style.display = searchRoomsBox.style.display == 'none' ? searchRoomsBox.style.display = 'flex' : searchRoomsBox.style.display = 'none';
 })
