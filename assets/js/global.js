c12704ef68b058039f70377409c91d4a9050edc5
Vinicius <vinicius.paiva2@outlook.com>
2024-12-10 10:48:46 -0300

    Adicionado sidebar global

--- assets/js/global.js
+++ assets/js/global.js
@@ -4,11 +4,14 @@ const body = document.getElementById('body');
 const favicon = "<link rel='shortcut icon' href='/assets/img/favicon_io/favicon.ico' type='image/x-icon'>"
 head.innerHTML += favicon;
 
+const bootstrapIcons = "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css'>";
+head.innerHTML += bootstrapIcons;
+
 const header = document.createElement('header');
 header.setAttribute('id', 'header');
 
 header.innerHTML +=
-"<a href='index.html'><img id='logo-header' src='/assets/img/logo.png' alt='logo'></a>" +
+"<a href='/index.html'><img id='logo-header' src='/assets/img/logo.png' alt='logo'></a>" +
 "<div id='navbar'>" +
     "<a class='navbar-items navitems-header' href=''>COMPRAR</a>" +
     "<a class='navbar-items navitems-header' href=''>ALUGAR</a>" +
@@ -19,8 +22,20 @@ header.innerHTML +=
 "</div>";
 body.prepend(header);
 
-const hamburguerIcon = document.getElementsByClassName('bi-list')[0];
+const sidebarContent = document.createElement('div');
+sidebarContent.setAttribute('id', 'sidebar');
+sidebarContent.setAttribute('style', 'display: none;');
+
+sidebarContent.innerHTML =
+    "<a class='navbar-items' href='''>COMPRAR</a>" +
+    "<a class='navbar-items' href=''>ALUGAR</a>" +
+    "<a class='navbar-items' href=''>VENDER</a>" +
+    "<a class='navbar-items' href=''>SOBRE NÓS</a>" +
+    "<a class='navbar-items' href='assets/html/contato.html'>CONTATO</a>";
+body.prepend(sidebarContent);
+
 const sidebar = document.getElementById('sidebar');
+const hamburguerIcon = document.getElementsByClassName('bi-list')[0];
 
 hamburguerIcon.addEventListener('click', () => {
     sidebar.style.display = sidebar.style.display == 'none' ? 'flex' : 'none';
