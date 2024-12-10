88b8960a7915b7d9f8abec4090be0091a8f4aecf
Vinicius <vinicius.paiva2@outlook.com>
2024-12-10 09:03:32 -0300

    Criando nova página contato

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,9 @@
+const head = document.getElementById('head');
+const body = document.getElementById('body');
+
+const favicon = "<link rel='shortcut icon' href='assets/img/favicon_io/favicon.ico' type='image/x-icon'>"
+head.innerHTML += favicon;
+
 const hamburguerIcon = document.getElementsByClassName('bi-list')[0];
 const sidebar = document.getElementById('sidebar');
 
