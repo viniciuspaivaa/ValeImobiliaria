adffc38644e1aae1057693ec673a97949fc95979
Vinicius <vinicius.paiva2@outlook.com>
2024-12-10 10:57:17 -0300

    Adicionado footer global

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,6 +1,9 @@
 const head = document.getElementById('head');
 const body = document.getElementById('body');
 
+const globalCss = "<link rel='stylesheet' href='/assets/css/global.css'>";
+head.innerHTML += globalCss;
+
 const favicon = "<link rel='shortcut icon' href='/assets/img/favicon_io/favicon.ico' type='image/x-icon'>"
 head.innerHTML += favicon;
 
@@ -17,7 +20,7 @@ header.innerHTML +=
     "<a class='navbar-items navitems-header' href=''>ALUGAR</a>" +
     "<a class='navbar-items navitems-header' href=''>VENDER</a>" +
     "<a class='navbar-items navitems-header' href=''>SOBRE NÓS</a>" +
-    "<a class='navbar-items navitems-header' href='assets/html/contato.html'>CONTATO</a>" +
+    "<a class='navbar-items navitems-header' href='/assets/html/contato.html'>CONTATO</a>" +
     "<i class='bi bi-list'></i>" +
 "</div>";
 body.prepend(header);
@@ -31,7 +34,7 @@ sidebarContent.innerHTML =
     "<a class='navbar-items' href=''>ALUGAR</a>" +
     "<a class='navbar-items' href=''>VENDER</a>" +
     "<a class='navbar-items' href=''>SOBRE NÓS</a>" +
-    "<a class='navbar-items' href='assets/html/contato.html'>CONTATO</a>";
+    "<a class='navbar-items' href='/assets/html/contato.html'>CONTATO</a>";
 body.prepend(sidebarContent);
 
 const sidebar = document.getElementById('sidebar');
@@ -40,3 +43,15 @@ const hamburguerIcon = document.getElementsByClassName('bi-list')[0];
 hamburguerIcon.addEventListener('click', () => {
     sidebar.style.display = sidebar.style.display == 'none' ? 'flex' : 'none';
 })
+
+const footer = document.createElement('footer');
+footer.setAttribute('id', 'footer');
+
+footer.innerHTML +=
+"<a href='/index.html'><img id='footer-logo' src='/assets/img/logo.png' alt='logo'></a>" +
+"<p id='rights'>&copy; 2024 Vale Imobiliária. Todos os direitos reservados.</p>" +
+"<div id='follow'>" +
+    "<p id='follow-text'>SIGA A VALE IMOBILIÁRIA</p>" +
+    "<i class='bi bi-instagram'></i>" +
+"</div>";
+body.appendChild(footer);
